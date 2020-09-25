a39b55e8eda662dfdd1f7b357dfc767a0f3340ff
Djandrew10 <adrcristian@gmail.com>
2020-09-25 04:40:25 -0400

    Constructor and Inheritance

--- JavaBasics2020/src/com/syntax/class16/CalculatorTest.java
+++ JavaBasics2020/src/com/syntax/class16/CalculatorTest.java
@@ -1,4 +1,4 @@
-package com.syntax.class16;
+ package com.syntax.class16;
 
 public class CalculatorTest {
 	//related to Calculator class
@@ -17,7 +17,7 @@ public static void main(String[] args) {
 	
 	c.mult(10, 10);
 	// c.mult(10); //CE: number of values must match number of parameters
-	// //c.mult(10.99, 9.99); CE: values must match type of parameters
+	// c.mult(10.99, 9.99); CompileError: values must match type of parameters
 
 }
 }
---
+++ JavaBasics2020/src/com/syntax/class19/Task.java
@@ -0,0 +1,19 @@
+package com.syntax.class19;
+
+public class Task {
+
+	
+	int[] array;
+	
+	protected static int sum(int[] array2) {
+		int total = 0;
+
+		for (int element : array2) {
+			total = total + element;
+		}
+
+		return total;
+	}
+	
+	
+	}
